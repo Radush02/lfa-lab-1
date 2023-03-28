000b7c53b82dc92ebabafe13676d6cf6014311ed
Radu Cordunianu <82465411+Radush02@users.noreply.github.com>
2023-03-28 11:38:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# AFD - Cordunianu Radu 133 
+
+Format fisier de intrare:
+
+alfabet
+
+stari
+
+stare initiala
+
+stari finale
+
+tranzitii
+
---
+++ main.py
@@ -0,0 +1,27 @@
+def verifica(input_string):
+    stare_curenta = stare_initiala
+    for c in input_string:
+        if c not in alfabet:
+            raise ValueError(f"Caracter invalid: {c}.")
+        stare_curenta = tranzitii[stare_curenta][c]
+    return stare_curenta in stari_finale
+
+f=open("input.txt","r")
+alfabet = f.readline().strip().split()
+stari = f.readline().strip().split()
+stare_initiala = f.readline().strip()
+stari_finale = f.readline().strip().split()
+tranzitii={}
+for index,sir in enumerate(f.readlines()):
+    string=sir.strip().split(sep=' ')
+    tranzitii[stari[index]]={k[:k.find(':')]:k[k.find(':')+1:] for k in string}
+
+
+
+print("Introduceti input")
+print(tranzitii)
+
+#input_strings = ['fmi','ffmm','fffmmfmifmfmf','ffmmiifmii']
+input_strings=input().strip().split()
+for s in input_strings:
+    print(f"{s}: {verifica(s)}")
